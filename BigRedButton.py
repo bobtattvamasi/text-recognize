b1bd800dbfa724bb70e3e22d939abbcc7a3075d6
bobtattvamasi <bob.tattvamasi@gmail.com>
2016-12-24 22:46:22 +0400

    Update BigRedButton.py

--- BigRedButton.py
+++ BigRedButton.py
@@ -1,1 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+import cv2
+import os
+
+
+from Tkinter import *
+
+#-----------------------------------------------------------------------
+
+class BigRedBut:
+	def __init__(self):
+		self.but = Button(root,
+						text="Старт",
+						width=30,height=5,
+						bg="red",fg="green")
+		self.but_1 = Button(root,
+						text="Test",
+						width=30,height=5,
+						bg="white",fg="green")
+		#обработчик события нажатия на кнопку мыши				
+		self.but.bind("<Button-1>", self.starts)
+		self.but_1.bind("<Button-1>", self.train)
+		#распаковывает нашу кнопку на область root
+		self.but.pack()
+		self.but_1.pack()
+		
+	def  starts(self,event):
+		execfile('GenDat.py')
+		
+	def train(self, event):
+		execfile('TrainAndTest.py')
+		
+#-----------------------------------------------------------------------
+
+#----------------------___main___---------------------------------------
+
+root = Tk()
+obj = BigRedBut()
+mainloop()		
 
