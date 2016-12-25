0426d2d7fb232b8b0bd080e6a752e2750906f241
bobtattvamasi <bob.tattvamasi@gmail.com>
2016-12-25 09:12:51 +0400

    Update BigRedButton.py

--- BigRedButton.py
+++ BigRedButton.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# BigRedButton.py
 
 import sys
 import numpy as np
@@ -8,36 +9,69 @@ import os
 
 from Tkinter import *
 
+MIN_CONTOUR_AREA = 100
+
 #-----------------------------------------------------------------------
 
 class BigRedBut:
 	def __init__(self):
-		self.but = Button(root,
+		
+		#-------------BUTTONS-----------------------
+		# Train button
+		self.but_train = Button(root,
 						text="Старт",
 						width=30,height=5,
 						bg="red",fg="green")
-		self.but_1 = Button(root,
+		
+		# Testing button
+		self.but_test = Button(root,
 						text="Test",
 						width=30,height=5,
 						bg="white",fg="green")
-		#обработчик события нажатия на кнопку мыши				
-		self.but.bind("<Button-1>", self.starts)
-		self.but_1.bind("<Button-1>", self.train)
-		#распаковывает нашу кнопку на область root
-		self.but.pack()
-		self.but_1.pack()
+		# Clean button
+		self.but_clean = Button(root,
+								text="Clean",
+								width=30,height=5,
+								bg="white",fg="green")
+		# Recognize button
+		self.but_rec = Button(root,
+								text="Recognize",
+								width=30,height=5,
+								bg="white",fg="green")
 		
-	def  starts(self,event):
-		execfile('GenDat.py')
+		#-------------HANDLER------------------------				
+		#обработчик события нажатия на кнопки мыши				
+		self.but_train.bind("<Button-1>", self.test)
+		self.but_test.bind("<Button-1>", self.train)
+		self.but_clean.bind("<Button-1>",self.clean)
+		self.but_rec.bind("<Button-1>",self.rec)
+		
+		#-------------PACK---------------------------
+		#распаковывает наши кнопки на область root
+		self.but_train.pack()
+		self.but_test.pack()
+		self.but_clean.pack()
+		self.but_rec.pack()
+		
+		
+	def  test(self,event):
+		execfile('GDat.py')
 		
 	def train(self, event):
 		execfile('TrainAndTest.py')
-		
+	
+	def clean(self,event):
+		pass
+	
+	def recognize(self,event):
+		pass
+	
+	def rec(self, event):
+		pass			
 #-----------------------------------------------------------------------
 
 #----------------------___main___---------------------------------------
 
 root = Tk()
 obj = BigRedBut()
-mainloop()		
-
+mainloop()	
